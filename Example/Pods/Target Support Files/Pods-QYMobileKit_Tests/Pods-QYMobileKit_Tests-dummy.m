420a499c09e5f4f9d2491ceb8261384ad37070a9
Peter Wen <peter.wen@razer.com>
2022-02-19 08:09:46 +0800

    Create mobile kit

---
+++ Example/Pods/Target Support Files/Pods-QYMobileKit_Tests/Pods-QYMobileKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QYMobileKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_QYMobileKit_Tests
+@end
---
+++ Example/Pods/Target Support Files/QYMobileKit/QYMobileKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_QYMobileKit : NSObject
+@end
+@implementation PodsDummy_QYMobileKit
+@end
